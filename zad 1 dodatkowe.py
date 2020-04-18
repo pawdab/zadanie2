7fabe7bd462b5e2e1c1221bdfbc53b9c24044525
Paweł Dąbrowski <paweldabrowski@zoho.com>
2020-04-18 22:59:56 +0200

    Change zad 1 dodatkowe

--- zad 1 dodatkowe.py
+++ zad 1 dodatkowe.py
@@ -1,26 +1,32 @@
 numbers = [5,32,56,2,2,16,7,10,23,100]
-numbers = [round(number+1e-15, -1) for number in numbers]
+numbers = [int(round(number+1e-15, -1)) for number in numbers]
 
 print(numbers)
 
-numbers.remove(min(numbers))
-numbers.remove(max(numbers))
+numbers = [number for number in numbers if number != min(numbers) and number != max(numbers)]
+
+#smallest = min(numbers)
+#biggest = max(numbers)
+
+#for number in numbers[:]:
+#    if smallest in numbers:
+#        numbers.remove(smallest)
+#    else:
+#        break
+#    if biggest in numbers:
+#        numbers.remove(biggest)
+#    else:
+#        break
 
 print(numbers)
-#for number in numbers:
-#    if number == max numbers:
-#        najmniejszanumbers.remove(number)
-    
+
 
 def mean(lista):
     average = 0
     for element in lista:
         average = average + element
-
-    return average/len(lista)
-
+    return round(average/len(lista),2)
 
 
-mean_number = mean(numbers)
-print(mean_number)
+print(mean(numbers))
 
